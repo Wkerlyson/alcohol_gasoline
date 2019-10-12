07ea7468a64b96eb3fdcd593860de4d3a57bef7d
Wkerlyson <wkerlyson.batista@gmail.com>
2019-10-12 00:11:39 -0300

    adding Submit Form

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:alcohol_gasoline/widgets/input.widget.dart';
 import 'package:alcohol_gasoline/widgets/loading-button.widget.dart';
 import 'package:alcohol_gasoline/widgets/logo.widget.dart';
+import 'package:alcohol_gasoline/widgets/submit-form.wodget.dart';
+import 'package:alcohol_gasoline/widgets/success.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
@@ -32,54 +34,12 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
-          Container(
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  'Compensa utilizar álcool',
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 40,
-                    fontFamily: 'Big Shoulders Display',
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                LoadingButton(
-                  busy: false,
-                  func: () {},
-                  text: 'CALCULAR NOVAMENTE',
-                  invert: true,
-                ),
-              ],
-            ),
+          SubmitForm(
+            alcCtrl: _alcCtrl,
+            gastCtrl: _gasCtrl,
+            submitFunc: () {},
+            busy: false,
           ),
-          Input(
-            ctrl: _gasCtrl,
-            label: 'Gasolina',
-          ),
-          Input(
-            ctrl: _alcCtrl,
-            label: 'Álcool',
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          LoadingButton(
-            busy: true,
-            func: () {},
-            text: 'CALCULAR',
-            invert: false,
-          )
         ],
       ),
     );
