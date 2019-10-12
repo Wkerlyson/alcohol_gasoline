f59e8dc9fff6c82fda8e52d11a02f11fd567152f
Wkerlyson <wkerlyson.batista@gmail.com>
2019-10-11 23:50:05 -0300

    adding Loading Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:alcohol_gasoline/widgets/input.widget.dart';
+import 'package:alcohol_gasoline/widgets/loading-button.widget.dart';
 import 'package:alcohol_gasoline/widgets/logo.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
@@ -31,6 +32,37 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
+          Container(
+            margin: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'Compensa utilizar álcool',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 40,
+                    fontFamily: 'Big Shoulders Display',
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                LoadingButton(
+                  busy: false,
+                  func: () {},
+                  text: 'CALCULAR NOVAMENTE',
+                  invert: true,
+                ),
+              ],
+            ),
+          ),
           Input(
             ctrl: _gasCtrl,
             label: 'Gasolina',
@@ -39,6 +71,15 @@ class HomePage extends StatelessWidget {
             ctrl: _alcCtrl,
             label: 'Álcool',
           ),
+          SizedBox(
+            height: 20,
+          ),
+          LoadingButton(
+            busy: true,
+            func: () {},
+            text: 'CALCULAR',
+            invert: false,
+          )
         ],
       ),
     );
